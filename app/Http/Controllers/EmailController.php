eb67ace6eec7866d90abe5e485164996f4d9212f
courage <couragesblog@gmail.com>
2022-09-29 14:01:16 +0100

    Add email notification logic
    - send email to recepients
    - use custom test email class
    - return json if successfull

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\EmailRequest;
+use App\Mail\TestEmail;
+use Illuminate\Support\Facades\Mail;
 
 class EmailController extends Controller
 {
     public function __invoke(EmailRequest $request)
     {
-        
+        Mail::to($request->only('to'))->send(
+            new TestEmail($request->except('email'))
+        );
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'sent successfullky!'
+        ]);
     }
 }
